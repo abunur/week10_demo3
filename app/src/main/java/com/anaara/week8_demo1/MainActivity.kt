c89ef527655e8b7148ab40d024bb049631571d4e
Fuad Kamal <fuad@anaara.com>
2018-04-18 20:54:44 -0400

    made a comment

---
+++ app/src/main/java/com/anaara/week8_demo1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.anaara.week8_demo1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+//TODO: write some code
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
